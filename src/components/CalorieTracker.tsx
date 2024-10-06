f7e1c7d5d2ade5774feb73db9ed61f77db85b335
francisco soto  flores <soto_flores98@hotmail.com>
2024-10-06 09:44:57 -0500

    migrando a contextAPI

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,18 +1,10 @@
-import { useMemo } from "react"
-import type { Activity } from "../types"
 import CalorieDisplay from "./CalorieDisplay"
+import { useActivity } from "../hooks/useActivity"
 
-type CalorieTrackerProps = {
-    activities: Activity[]
-}
+export default function CalorieTracker() {
 
-export default function CalorieTracker({activities} : CalorieTrackerProps) {
+    const { caloriesConsumed, caloriesBurned, netCalories } = useActivity();
 
-    // Contadores
-    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
-    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
-    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
-    
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
